12710501b76f73b9d3a187223cbddac4826e5e38
Moritz Miedler <miedler.m03@htlwienwest.at>
2021-05-19 11:30:23 +0200

    i live in spain but the s is silent

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const asyncHandler = require('express-async-handler');
 const router = express.Router();
 
 const { getAllCocktails,getCocktailByName,getCocktailCheaperThan,} = require('../model/functions.js');
-// const asyncHandler = require('express-async-handler');
+
 
 
 
@@ -20,6 +20,14 @@ router.get(
   '/cocktails/:name/zutaten',
   asyncHandler(async (req, res) => {
     const result = await getCocktailByName(req.params.name);
+    res.status(result.status).send(result.data);
+  }),
+);
+
+router.get(
+  '/cocktails/:price',
+  asyncHandler(async (req, res) => {
+    const result = await getCocktailCheaperThan(req.params.price);
     res.status(result.status).send(result.data);
   }),
 );
